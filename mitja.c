b107b59f061be7d3d9ec9a9776be06d4afdfb0ea
joalda1718daw2vib <joalda1718daw2vib@gmail.com>
2018-05-03 08:53:55 +0200

    Primera versió

---
+++ mitja.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+float suma=0;
+float mitja=0;
+float nou_valor;
+int numero_repeticions=0;
+char consulta='s';
+while(consulta=='s'){
+printf("Dóna la següent dada: ");
+suma=suma+nou_valor;
+numero_repeticions=numero_repeticions+1;
+printf("Vols continuar s/n: ");
+scanf("%c",&consulta);
+}
+mitja=suma/numero_repeticions;
+printf("La mitja resultat és: %.2f\n",mitja);
+return 0;
+}
